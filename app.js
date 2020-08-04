b5febc13db7f3b2258815c171613e7829f679802
drogan120 <alimahmudin.hp@gmail.com>
2020-08-04 20:54:38 +0700

    restructure folder and pug layout

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
-const dotEnv = require("dotenv");
 
 require("./config/config");
 
+// load templare engine
+app.set("views", path.join(__dirname, "app/views"));
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send({ messages: "hello world" });
+  res.render("index");
 });
+
+app.get("/articles", (req, res) => {
+  res.render("articles/index");
+});
+
 app.listen(process.env.APP_PORT, () => {
   console.log("Server is running on http://127.0.0.1:" + process.env.APP_PORT);
 });
