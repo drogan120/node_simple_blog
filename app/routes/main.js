61ae7c81fc012c1f2c17853f74640d4688d0a5f3
drogan120 <alimahmudin.hp@gmail.com>
2020-08-07 23:04:41 +0700

    adding bootstrap and setup frontend

--- app/routes/main.js
+++ app/routes/main.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+const Post = require("../models/postModels");
+
 router.get("/", (req, res) => {
-  res.render("main/index");
+  Post.find({}, (err, data) => {
+    if (err) {
+      res.send({ error: err });
+    } else {
+      res.render("main/index", { articles: data });
+      console.log(data);
+    }
+  });
+});
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id, (err, data) => {
+    res.send({ articles: data });
+  });
 });
 
 module.exports = router;
