3fc9b2ac40f2424a369fb73fdb25628a060bf78d
drogan120 <alimahmudin.hp@gmail.com>
2020-09-30 22:51:44 +0700

    adding register functionality

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 let User = require("../models/userModel");
 
@@ -7,9 +8,31 @@ let User = require("../models/userModel");
 router.get("/register", (req, res) => {
   res.render("auth/register");
 });
-
+// Login form
 router.get("/login", (req, res) => {
   res.render("auth/login");
+});
+
+// User List
+router.get("/", (req, res) => {
+  res.render("user/index");
+});
+// Save Post to database
+router.post("/", (req, res) => {
+  let user = new User();
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.username = req.body.username;
+  user.password = req.body.password;
+
+  user.save((err) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect("/user/login");
+    }
+  });
 });
 
 module.exports = router;
